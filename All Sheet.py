a3f26590a3abd3b5b520b4407f6e91ffe3b6f88c
Akash Chachere <88659038+akashchachere@users.noreply.github.com>
2021-08-09 16:00:56 +0530

    Update All Sheet.py

--- All Sheet.py
+++ All Sheet.py
@@ -16,18 +16,3 @@ for sheet in c.sheet_names:
 
     d.to_csv('C:\\Users\\sony\\Desktop\\Excel Sheet\\AllSheet.csv', encoding='utf-8', index=True, header=True)
     print(d)
-
-
-'''
-name = fixed_data['Sample ID'].unique()
-number = fixed_data.shape[0]
-temp_list = pd.DataFrame(
-    {'ids': name,
-     'nums': number,
-    })
-
-fileAppendName1 = concon + "FREQ.FREQ"
-with open(fileAppendName1, 'a') as f:
-    temp_list.to_csv(f, header=False, index = False)  
-
-'''
